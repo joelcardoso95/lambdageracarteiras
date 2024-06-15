e5ad72045bb762342d7bff8d7e4b5c8d7aca3790
joelcardoso95 <joelmarcos.cardoso@aol.com>
2024-06-15 15:42:09 -0300

    teste pdf 2

--- subo.bat
+++ subo.bat
@@ -1,10 +1,9 @@
 git add .
-git commit -m "teste pdf"
+git commit -m "teste pdf 2"
 git push
 set GOOS=linux
 set GOARCH=arm64
 set CGO_ENABLED=0
-sudo apt install wkhtmltopdf
 go build -o bootstrap main.go
 del main.zip
 tar.exe -a -cf main.zip bootstrap
